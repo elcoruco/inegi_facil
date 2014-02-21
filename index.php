fca15fa0328a710eaefaed1fff6c1ebdee443a9c
el coruco <el.coruco@gmail.com>
2014-02-21 10:43:40 -0600

    just start the thing

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <p id="log"></p>
+  <script src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,16 @@
+var plugin = function(form, adjust, debug){
+  var debug = debug || false;
+  // check if google is avaliable
+  try{
+  }
+  catch(e){
+  }
+
+  // check if form is avaliable
+  try{
+  }
+  catch(e){
+  }
+
+  //check if geolocalization is avaliable
+};
